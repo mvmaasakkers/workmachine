7cc86b716024b92d5d52061b8eb3f79a067d4fc0
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2026-07-20 12:23:31 +0200

    Install Neovim from official release tarball on Linux

--- roles/nvim/files/lua/config/lazy.lua
+++ roles/nvim/files/lua/config/lazy.lua
@@ -3,12 +3,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
+    -- no getchar() here: this config is provisioned via headless nvim, where waiting for a key hangs forever
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
       { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
     }, true, {})
-    vim.fn.getchar()
     os.exit(1)
   end
 end
